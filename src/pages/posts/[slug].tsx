35fcd992d35cfa44c67afc9943eb4c5b777618fe
OJailson17 <JaylsonO17@gmail.com>
2022-02-09 02:20:09 -0300

    fix type post problem

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,10 +1,10 @@
-import Head from "next/head";
-import { GetServerSideProps } from "next";
-import { getSession } from "next-auth/client";
-import { RichText } from "prismic-dom";
-import { getPrismicClient } from "../../services/prismic";
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+import { getSession } from 'next-auth/client';
+import { RichText } from 'prismic-dom';
+import { getPrismicClient } from '../../services/prismic';
 
-import styles from "./post.module.scss";
+import styles from './post.module.scss';
 
 interface PostProps {
   post: {
@@ -13,6 +13,24 @@ interface PostProps {
     content: string;
     updatedAt: string;
   };
+}
+
+interface PostDataResponse {
+  data: {
+    title: [
+      {
+        type: string;
+        text: string;
+      },
+    ];
+    content: [
+      {
+        type: string;
+        text: string;
+      },
+    ];
+  };
+  last_publication_date: string;
 }
 
 export default function Post({ post }: PostProps) {
@@ -55,19 +73,23 @@ export const getServerSideProps: GetServerSideProps = async ({
 
   const prismic = getPrismicClient(req);
 
-  const response = await prismic.getByUID("publication", String(slug), {});
+  const response: PostDataResponse = await prismic.getByUID(
+    'publication',
+    String(slug),
+    {},
+  );
 
   const post = {
     slug,
     title: RichText.asText(response.data.title),
     content: RichText.asHtml(response.data.content),
     updatedAt: new Date(response.last_publication_date).toLocaleDateString(
-      "pt-BR",
+      'pt-BR',
       {
-        day: "2-digit",
-        month: "long",
-        year: "numeric",
-      }
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      },
     ),
   };
 
